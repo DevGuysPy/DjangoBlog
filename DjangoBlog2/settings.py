3f6e6bda0dddf5fd95ee55d9f4b9bf23c522ce55
Dima <dimckuk322@gmail.com>
2015-06-07 14:25:30 +0300

    Added new application

--- DjangoBlog2/settings.py
+++ DjangoBlog2/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'DjangoBlog2',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -55,7 +56,7 @@ ROOT_URLCONF = 'DjangoBlog2.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
