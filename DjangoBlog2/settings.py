02f9bea2ab7fcb4ce03c51b3a3a83270c2702eed
Dima <dimckuk322@gmail.com>
2015-06-07 14:35:21 +0300

    Revert "Added new application"
    This reverts commit c11f33290d3b74f4f251fb1ac4a0c0ce1620fd9e.

--- DjangoBlog2/settings.py
+++ DjangoBlog2/settings.py
@@ -101,8 +101,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
-LOGIN_REDIRECT_URL = '/'
+TEMPLATE_DIRS = (
+    os.path.join(BASE_DIR,  'templates'),
+)
