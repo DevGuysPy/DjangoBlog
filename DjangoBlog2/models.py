832b34de0229ca8e640e0248d5138f8242cb2748
Denis OH <d.halenok@gmail.com>
2015-06-07 20:58:15 +0300

    Added author field to models

--- DjangoBlog2/models.py
+++ DjangoBlog2/models.py
@@ -1,6 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
+	author = models.ForeignKey(User)
 	title = models.CharField(max_length=100)
 	text = models.TextField()
 	date = models.DateTimeField(auto_now_add=True)
