ecc34df84cbe051e4454f9ccb54f4f5743553812
Denis OH <d.halenok@gmail.com>
2015-06-07 21:38:38 +0300

    Updated migrations

--- DjangoBlog2/migrations/0001_initial.py
+++ DjangoBlog2/migrations/0001_initial.py
@@ -2,22 +2,25 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='Article',
             fields=[
-                ('id', models.AutoField(primary_key=True, serialize=False, auto_created=True, verbose_name='ID')),
-                ('article_title', models.CharField(max_length=100)),
-                ('article_text', models.TextField()),
-                ('article_date', models.DateTimeField(auto_now_add=True)),
-                ('article_likes', models.IntegerField(default=0)),
+                ('id', models.AutoField(serialize=False, primary_key=True, auto_created=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('text', models.TextField()),
+                ('date', models.DateTimeField(auto_now_add=True)),
+                ('likes', models.IntegerField(default=0)),
+                ('author', models.ForeignKey(to=settings.AUTH_USER_MODEL)),
             ],
             options={
                 'db_table': 'articles',
