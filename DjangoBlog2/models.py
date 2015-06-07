682d9ff9353b95bb8a49f4d38d5fbb1ca91593f5
Dima <dimckuk322@gmail.com>
2015-06-07 15:01:59 +0300

    fixed modles to models

--- DjangoBlog2/models.py
+++ DjangoBlog2/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Message(models.Model):
-	article_title = models.CharField(max_length= 100)
-	article_text = modles.TextField()
+	article_title = models.CharField(max_length=100)
+	article_text = models.TextField()
 	article_date = models.DateTimeField()
-	article_likes = models.IntegerField()
+	article_likes = models.IntegerField(default=0)
 
 	class Meta():
 		db_table = 'articles'
